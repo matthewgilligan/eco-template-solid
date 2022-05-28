d91fa8ef5468768f6c3e4173ddf27585f47d9d21
Matthew Gilligan <mcgilligan97@gmail.com>
2022-05-28 17:31:14 +0100

    Fill out seo component

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,14 +1,35 @@
-import { mergeProps } from 'solid-js';
-import { Title, Link, Meta } from 'solid-meta';
+import { Title, Meta } from 'solid-meta';
+import { useLocation } from 'solid-app-router';
 
-export default function SEO(props) {
-  const merged = mergeProps({ title: 'Eco Template' }, props);
+export default function SEO({ title, description, image, type }) {
+  const { pathname } = useLocation();
 
-  console.log(merged);
+  const siteUrl = 'https://google.com';
+
+  const seo = {
+    title: title || 'Eco Template',
+    description: description,
+    image: `${siteUrl}${image}`,
+    url: `${siteUrl}${pathname}`,
+    type: type || 'website',
+    twitterUsername: 'twitter-username',
+  };
 
   return (
     <>
-      <Title>{merged.title}</Title>
+      <Title>{title}</Title>
+      <Meta name='description' content={seo.description} />
+      <Meta name='image' content={seo.image} />
+      <Meta property='og:url' content={seo.url} />
+      <Meta property='og:type' content={seo.url} />
+      <Meta property='og:title' content={seo.title} />
+      <Meta property='og:description' content={seo.description} />
+      <Meta property='og:image' content={seo.type} />
+      <Meta property='twitter:card' content='summary_large_image' />
+      <Meta property='twitter:creator' content={seo.twitterUsername} />
+      <Meta property='twitter:title' content={seo.title} />
+      <Meta property='twitter:description' content={seo.description} />
+      <Meta property='twitter:image' content={seo.image} />
     </>
   );
 }
